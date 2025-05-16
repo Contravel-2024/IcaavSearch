62c6e51e33fb9fd204896653506eb43d3dea4198
Contravel <71468080+Contravel@users.noreply.github.com>
2025-05-15 19:02:38 -0600

    Update formasPagos.php

--- backend/formasPagos.php
+++ backend/formasPagos.php
@@ -5,8 +5,8 @@ use Funciones\SYBASE;
 include_once "vendor/autoload.php";
 include_once "cors.php";
 
-$data = "Hola";
-echo $data;
-// $data = SYBASE::obtenerFormasPagos();
-
+// $data = "Hola";
 // echo $data;
+$data = SYBASE::obtenerFormasPagos();
+
+echo $data;
